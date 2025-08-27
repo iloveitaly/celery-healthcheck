b2b3001fd15432af3dd0a69c1cef67e8921dc6f6
Micah Smith <micahjsmith@gmail.com>
2025-08-27 12:08:31 -0400

    Healthcheck self only

--- celery_healthcheck/server.py
+++ celery_healthcheck/server.py
@@ -2,39 +2,65 @@ import threading
 
 import uvicorn
 from celery import bootsteps
-from fastapi import FastAPI
+from celery.worker import WorkController
+from fastapi import FastAPI, status
 from fastapi.responses import JSONResponse
 
 app = FastAPI()
 
+HEALTHCHECK_DEFAULT_PORT = 9000
+HEALTHCHECK_DEFAULT_PING_TIMEOUT = 2.0
+
 
 class HealthCheckServer(bootsteps.StartStopStep):
-    def __init__(self, worker, **kwargs):
-        self.worker = worker
 
-        self.app = app
+    def __init__(self, parent: WorkController, **kwargs):
         self.thread = None
 
-    def start(self, worker):
+        # facilitates testing
+        self.app = app
+
+        # config
+        self.healthcheck_port = int(
+            getattr(parent.app.conf, "healthcheck_port", HEALTHCHECK_DEFAULT_PORT)
+        )
+        self.healthcheck_ping_timeout = float(
+            getattr(
+                parent.app.conf,
+                "healthcheck_ping_timeout",
+                HEALTHCHECK_DEFAULT_PING_TIMEOUT,
+            )
+        )
+
+    def start(self, parent: WorkController):
+
         @self.app.get("/")
         async def celery_ping():
-            insp = worker.app.control.inspect()
+            insp = parent.app.control.inspect(
+                destination=parent.hostname, timeout=self.healthcheck_ping_timeout
+            )
             result = insp.ping()
 
             if result:
                 return JSONResponse(
-                    content={"status": "ok", "result": result}, status_code=200
+                    content={"status": "ok", "result": result},
+                    status_code=status.HTTP_200_OK,
                 )
             else:
                 return JSONResponse(
-                    content={"status": "error", "result": result}, status_code=503
+                    content={"status": "error", "result": result},
+                    status_code=status.HTTP_503_SERVICE_UNAVAILABLE,
                 )
 
         def run_server():
-            uvicorn.run(self.app, host="0.0.0.0", port=9000)
+            uvicorn.run(
+                self.app,
+                host="0.0.0.0",
+                port=self.healthcheck_port,
+            )
 
         self.thread = threading.Thread(target=run_server, daemon=True)
         self.thread.start()
 
-    def stop(self, worker):
+    def stop(self, parent: WorkController):
         pass
